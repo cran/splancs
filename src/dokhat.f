824a96a9c19ca5f79701836a990860c225b9b137
Roger Bivand <Roger.Bivand@nhh.no>
2002-07-29 00:00:00 +0000

    version 2.01-9

--- src/dokhat.f
+++ src/dokhat.f
@@ -1,26 +1,15 @@
 C Copyright Barry Rowlingson <b.rowlingson@lancaster.ac.uk> and 
 C Peter Diggle (c) 1991-3; http://www.maths.lancs.ac.uk/~rowlings/Splancs/
-C R port: copyright 1998-2000 by Roger S. Bivand
-C
-C  This program is free software; you can redistribute it and/or modify
-C  it under the terms of the GNU General Public License as published by
-C  the Free Software Foundation; either version 2 of the License, or
-C  (at your option) any later version.
-C
-C  This program is distributed in the hope that it will be useful,
-C  but WITHOUT ANY WARRANTY; without even the implied warranty of
-C  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-C  GNU General Public License for more details.
+C R port: copyright 1998-2002 by Roger S. Bivand
 
-
-
-      subroutine dokhat(x,y,n,xp,yp,np,s,ns,hkhat)
+      subroutine trykh(x,y,n,xp,yp,np,s,ns,hkhat,icounts,hkhats,nptns)
 
       implicit real*8(a-h,o-z)
 
       include 'bounds.cmn'
 
       dimension x(n),y(n),xp(np+1),yp(np+1),s(ns),hkhat(ns)
+      dimension icounts(nptns), hkhats(nptns)
 
       area = plarea(xp,yp,np)
 
@@ -31,6 +20,7 @@ C  GNU General Public License for more details.
       do i=1,ns
         hkhat(i)=0.0d0
       end do
+
 
       do i=2,n
         i1=i-1                                                                
@@ -48,23 +38,37 @@ C  GNU General Public License for more details.
             if(it.le.ns) then
               wij=weight(xi,yi,t,xp,yp,np)
               wji=weight(x(j),y(j),t,xp,yp,np)
-            end if
 
-            hkhat(it)=hkhat(it)+wij+wji
+              hkhat(it)=hkhat(it)+wij+wji
+              ipos=n*(it-1)
+              hkhats(i+ipos)=hkhats(i+ipos) + wij
+              hkhats(j+ipos)=hkhats(j+ipos) + wji
+              icounts(i+ipos)=icounts(i+ipos) + 1
+              icounts(j+ipos)=icounts(j+ipos) + 1
+            end if
           end if
         end do
       end do
 
       do i=2,ns
         hkhat(i)=hkhat(i)+hkhat(i-1)
+        do j=1,n
+          jj=j+(n*(i-1))
+          jj1=j+(n*(i-2))
+          hkhats(jj)=hkhats(jj)+hkhats(jj1)
+        end do
       end do
 
       dn=dfloat(n)*dfloat(n-1)
+      adn=area/dn
 
       do i=1,ns                                              
-        hkhat(i)=hkhat(i)*area/dn
+        hkhat(i)=hkhat(i)*adn
+        do j=1,n
+          jj=j+(n*(i-1))
+          hkhats(jj)=hkhats(jj)*adn
+        end do
       end do
 
       return                                                                
       end
-
